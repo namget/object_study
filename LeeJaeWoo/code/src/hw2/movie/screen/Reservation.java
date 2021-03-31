e0bfb145b148a8e3f5115063090577260d90fde9
namget <yeboykr@naver.com>
2021-04-01 00:16:58 +0900

    complete ep2

--- LeeJaeWoo/code/src/hw2/movie/screen/Reservation.java
+++ LeeJaeWoo/code/src/hw2/movie/screen/Reservation.java
@@ -1,18 +1,61 @@
-package hw2.movie.screen;
+package hw2.ticket;
 
-import hw2.item.Money;
-import hw2.user.Customer;
+import hw2.movie.screen.Screening;
+import hw2.user.User;
+
+import java.util.List;
 
 public class Reservation {
-
-    private Customer customer;
+    private List<User> users;
     private Screening screening;
-    private Money money;
 
-
-    public Reservation(Customer customer, Screening screening, Money money) {
-        this.customer = customer;
+    public Reservation(List<User> users, Screening screening){
+        this.users = users;
         this.screening = screening;
-        this.money = money;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public boolean isAndAboveUserCount(int count){
+        return users.size() >= count;
+    }
+
+    public boolean isAboveAge(int age){
+        for(User user : users){
+            return user.getAge() > age;
+        }
+        return false;
+    }
+
+    public boolean isAndAboveAge(int age){
+        for(User user : users){
+            return user.getAge() >= age;
+        }
+        return false;
+    }
+
+
+    public boolean isAndUnderAge(int age){
+        for(User user : users){
+            return user.getAge() <= age;
+        }
+        return false;
+    }
+
+    public boolean isUnderAge(int age){
+        for(User user : users){
+            return user.getAge() < age;
+        }
+        return false;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public int getSequence(){
+        return screening.getSequence();
     }
 }
